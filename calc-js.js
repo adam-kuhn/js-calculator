37b7aa694b191f4c223b1ce818b267c398286981
adam-kuhn <adam.kuhn20@gmail.com>
2017-11-21 15:00:35 +1300

    confirm ability to display a # on screen id

--- calc-js.js
+++ calc-js.js
@@ -1,8 +1,21 @@
 window.onload = function (){
-document.getElementById("calc-btns").addEventListener("click", change);
+document.getElementById("calc-btns").addEventListener("click", display);
+
+let digits = document.getElementsByClassName("nbr-btn")
+let operator = document.getElementsByClassName("opr-btn")
 }
 
-function change(evt) {
-  evt.target.classList.add('button-blue');
+function display() {
+  document.getElementById("calc-screen").innerHTML = 1
 
 }
+
+
+
+  /*evt.target.getElementById(i+1 stringify)
+
+
+  let display = "0"
+for (i=0; i < digits.length; i++){
+  display = i
+}*/
