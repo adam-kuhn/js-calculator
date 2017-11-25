882409b3b6ec9250b1973d59b54238338d517410
adam-kuhn <adam.kuhn20@gmail.com>
2017-11-26 11:27:51 +1300

    all number btn and functions to calc

--- calc-js.js
+++ calc-js.js
@@ -5,6 +5,13 @@ document.getElementById("0").addEventListener("click", display0);
 document.getElementById("1").addEventListener("click", display1);
 document.getElementById("2").addEventListener("click", display2);
 document.getElementById("3").addEventListener("click", display3);
+document.getElementById("4").addEventListener("click", display4);
+document.getElementById("5").addEventListener("click", display5);
+document.getElementById("6").addEventListener("click", display6);
+document.getElementById("7").addEventListener("click", display7);
+document.getElementById("8").addEventListener("click", display8);
+document.getElementById("9").addEventListener("click", display9);
+
 document.getElementById("plus").addEventListener("click", displayPlus);
 document.getElementById("minus").addEventListener("click", displayMinus);
 document.getElementById("times").addEventListener("click", displayTimes);
@@ -54,6 +61,43 @@ function display3(){
   displayEquation()
 }
 
+function display4(){
+  document.getElementById("calc-screen").innerHTML = 4
+  equation.push(4)
+  displayEquation()
+}
+
+function display5(){
+  document.getElementById("calc-screen").innerHTML = 5
+  equation.push(5)
+  displayEquation()
+}
+
+function display6(){
+  document.getElementById("calc-screen").innerHTML = 6
+  equation.push(6)
+  displayEquation()
+}
+
+function display7(){
+  document.getElementById("calc-screen").innerHTML = 7
+  equation.push(7)
+  displayEquation()
+}
+
+function display8(){
+  document.getElementById("calc-screen").innerHTML = 8
+  equation.push(8)
+  displayEquation()
+}
+
+function display9(){
+  document.getElementById("calc-screen").innerHTML = 9
+  equation.push(9)
+  displayEquation()
+}
+
+//operator functions
 function displayPlus(){
   document.getElementById("calc-screen").innerHTML = "+"
   equation.push("+")
